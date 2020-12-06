78de082a08c3e3ae754034cde66f08ccd8c4eb75
mern4 <mernproject4@gmail.com>
2020-12-06 23:39:39 +0530

    mern

---
+++ client/build/precache-manifest.b26dcd3d7dc28d1ea9533eea0e9e41e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41531ab6e4443620585f4719b1d1715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f8947718bc6a241a040",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "310c4d5f4c4b65520d4f",
+    "url": "/static/js/2.e9337927.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e9337927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8947718bc6a241a040",
+    "url": "/static/js/main.2d716f73.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
